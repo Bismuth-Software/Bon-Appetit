c81af3a23fa61e37831bb89d56bae0da553f9a34
ascenstohe4vn <128644895+ascenstohe4vn@users.noreply.github.com>
2025-11-04 15:20:45 -0500

    corn activity

--- src/main/java/net/bi83/bonappetit/core/BABlocks.java
+++ src/main/java/net/bi83/bonappetit/core/BABlocks.java
@@ -18,10 +18,11 @@ import java.util.function.Supplier;
 public class BABlocks {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(BonAppetit.ID);
 
-    public static final DeferredBlock<Block> CORN_BASE = registerBlock("corn_base", () -> new CornCropBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.WHEAT).randomTicks().offsetType(BlockBehaviour.OffsetType.NONE).instabreak().sound(SoundType.CROP)));
-    public static final DeferredBlock<Block> CORN_TOP = registerBlock("corn_top", () -> new CornCropBlockTop(BlockBehaviour.Properties.ofFullCopy(Blocks.WHEAT)));
+    public static final DeferredBlock<Block> CORN_BASE = registerBlockNoItem("corn_base", () -> new CornCropBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.WHEAT).randomTicks().offsetType(BlockBehaviour.OffsetType.NONE).instabreak().sound(SoundType.CROP)));
+    public static final DeferredBlock<Block> CORN_TOP = registerBlockNoItem("corn_top", () -> new CornCropBlockTop(BlockBehaviour.Properties.ofFullCopy(Blocks.WHEAT)));
 
     private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {DeferredBlock<T> toReturn = BLOCKS.register(name, block); registerBlockItem(name, toReturn); return toReturn;}
+    private static <T extends Block> DeferredBlock<T> registerBlockNoItem(String name, Supplier<T> block) {DeferredBlock<T> toReturn = BLOCKS.register(name, block); return toReturn;}
     private static <T extends Block> void registerBlockItem(String name, DeferredBlock<T> block) {BAItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties())); }
-    public static void register(IEventBus eventBus) {BLOCKS.register(eventBus); }
+    public static void register(IEventBus eventBus) {BLOCKS.register(eventBus);}
 }
